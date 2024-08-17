32a189fbd9c7adc8168332077f67d06d46159861
IsraelSouza <israel.felipe1991@gmail.com>
2024-08-17 08:43:00 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using BootcampCodingTheFutureXPInc_Full_Stack_Developer_DIO.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Israel";
+pessoa1.Idade = 26;
+pessoa1.Apresentar();
+
+Pessoa pessoa_FisicaRepresentacao =  new Pessoa();
