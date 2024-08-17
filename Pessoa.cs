279ef9bc00e3a6623b361bad2a67669825b96258
IsraelSouza <israel.felipe1991@gmail.com>
2024-08-17 08:43:04 -0300

    Create Pessoa.cs

---
+++ Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampCodingTheFutureXPInc_Full_Stack_Developer_DIO
+{
+    public class Pessoa
+    {
+        
+    }
+}
