34dd2df20f1f5f27e31c5edf4a3fb30e8b72a94d
IsraelSouza <israel.felipe1991@gmail.com>
2024-08-20 04:36:00 -0300

    Update Pessoa.cs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,9 +7,9 @@ namespace BootcampCodingTheFutureXPInc_Full_Stack_Developer_DIO.Models
 {
     public class Pessoa
     {
-        public string Nome {get;set;}
+        public string? Nome {get;set;}
         public int Idade {get;set;}
-        public string NomeRespresentanteLegalDaPessoaFisica{get;set;}
+        public string? NomeRespresentanteLegalDaPessoaFisica {get;set;}
 
         public void Apresentar(){
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
