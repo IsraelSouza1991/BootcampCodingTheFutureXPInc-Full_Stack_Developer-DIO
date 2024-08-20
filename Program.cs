ec70ecc43c06679702a19937a5dc03be37e56e69
IsraelSouza <israel.felipe1991@gmail.com>
2024-08-20 03:16:40 -0300

    Atualização

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿using BootcampCodingTheFutureXPInc_Full_Stack_Developer_DIO.Models;
+﻿//using BootcampCodingTheFutureXPInc_Full_Stack_Developer_DIO.Models;
 
-Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "Israel";
-pessoa1.Idade = 26;
-pessoa1.Apresentar();
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
 
-Pessoa pessoa_FisicaRepresentacao =  new Pessoa();
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Israel";
+// pessoa1.Idade = 26;
+// pessoa1.Apresentar();
+
+// Pessoa pessoa_FisicaRepresentacao =  new Pessoa();
