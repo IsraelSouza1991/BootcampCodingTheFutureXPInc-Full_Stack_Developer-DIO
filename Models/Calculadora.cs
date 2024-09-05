0f16dc18954dfc7d99ac9daa0c9f8bec83d6b42b
Israel Felipe de Oliveira Souza <israel.felipe1991@gmail.com>
2024-09-05 05:44:42 -0300

    Create Calculadora.cs

---
+++ Models/Calculadora.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampCodingTheFutureXPInc_Full_Stack_Developer_DIO.Models
+{
+    public class Calculadora
+    {
+        public void Somar()
+        {
+            
+        }
+    }
+}
