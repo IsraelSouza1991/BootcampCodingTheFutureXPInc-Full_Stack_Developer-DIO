8e8f81865796e9139ad5d74206462f3fd7c03b12
Israel Felipe de Oliveira Souza <israel.felipe1991@gmail.com>
2024-09-10 03:21:26 -0300

    Update Calculadora.cs

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -7,9 +7,21 @@ namespace BootcampCodingTheFutureXPInc_Full_Stack_Developer_DIO.Models
 {
     public class Calculadora
     {
-        public void Somar()
+        public void Somar(int x, int y)
         {
-            
+            Console.WriteLine($"{x} + {y} = {x+y}");            
+        }
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x-y}");            
+        }
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x*y}");            
+        }
+        public void dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} ÷ {y} = {x/y}");            
         }
     }
 }
